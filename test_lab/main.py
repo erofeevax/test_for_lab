179757f3853e932c0ba773b336db4cd74107d6f5
Alexander Erofeev <95953987+erofeevax@users.noreply.github.com>
2024-02-14 17:12:20 +0300

    Update main.py

--- test_lab/main.py
+++ test_lab/main.py
@@ -4,18 +4,22 @@ def find_summ(target):
     numbers = list(range(9, -1, -1))
     operators = ['+', '-', '']
 
+    result = []
+
     for i in product(operators, repeat=len(numbers) - 1):
         equation = ''.join(str(num) + op for num, op in zip(numbers, i)) + str(numbers[-1])
         if eval(equation) == target:
-            return equation
+            result.append(equation)
 
-    return None
+    return result
 # print('Введите конечную сумму')
 # target_sum = int(input())
 target_sum = 200
 result = find_summ(target_sum)
 
 if result:
-    print('Решение:', result, '=', target_sum)
+    print('Решения:')
+    for all in result:
+     print(all, '=', target_sum)
 else:
     print('Решение невозможно.')
